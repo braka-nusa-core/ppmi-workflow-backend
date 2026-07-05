451762467c629d3bc8a2865361f7739441322b6c
gufronnakaaw <gufronnakaaw@gmail.com>
2026-07-05 18:31:38 +0700

    feat: add metadata check

--- src/common/pipes/zod.pipe.ts
+++ src/common/pipes/zod.pipe.ts
@@ -5,6 +5,10 @@ export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodType) {}
 
   transform(value: unknown, metadata: ArgumentMetadata) {
+    if (metadata.type !== 'body') {
+      return value;
+    }
+
     try {
       return this.schema.parse(value);
     } catch (error) {
