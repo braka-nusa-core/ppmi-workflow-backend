67022197dd322aa5994e08667b6d324e4b632066
gufronnakaaw <gufronnakaaw@gmail.com>
2026-07-25 00:35:17 +0700

    feat: add new properties for credentials

--- src/common/types/express.d.ts
+++ src/common/types/express.d.ts
@@ -3,11 +3,6 @@ import 'express';
 declare module 'express' {
   export interface Request {
     fullurl: string;
-    credentials: {
-      id: string;
-      fullname: string;
-      roles?: string[];
-      is_admin: boolean;
-    };
+    credentials: { sub: string; fullname: string; role: 'SUPERADMIN' | 'USER' };
   }
 }
